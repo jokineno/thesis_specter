6de0fdfebb44b68048291d542c2d71d9cbccf538
Olli Jokinen <olli.jokinen@snowfox.ai>
2023-04-14 11:52:20 +0200

    [THESIS-2] full and demo pipelines

--- 3-create-training-files-thesis.sh
+++ 3-create-training-files-thesis.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -x
-echo "Creating training files (thesis version).. "
-python specter/data_utils/create_training_files.py \
---data-dir data/training \
---metadata data/training/metadata.json \
---outdir data/preprocessed/
-echo "Done. See results is data/preprocessed/"
 
+echo "Creating training files (thesis version).. "
+
+if [[ ! $* == *--demo* ]]
+then
+
+  echo "Using full data for training..."
+  python specter/data_utils/create_training_files.py \
+    --data-dir thesis_data/training \
+    --metadata thesis_data/training/metadata.json \
+    --outdir thesis_data/preprocessed/
+  echo "Done. See results is thesis_data/preprocessed/"
+
+else
+
+  echo "Using demo samples for training..."
+  python specter/data_utils/create_training_files.py \
+    --data-dir thesis_data/training_demo/ \
+    --metadata thesis_data/training_demo/metadata.json \
+    --outdir thesis_data/preprocessed_demo/
+  echo "Done. See results is thesis_data/preprocessed_demo/"
+fi
+
+echo "Done. Exiting.."
---
+++ 4-run-training-script-thesis.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Starting training script..."
+
+echo "Creating training files (thesis version).. "
+
+if [[ ! $* == *--demo* ]]
+then
+
+  echo "Using all the samples for training.. ====[THIS IS THE FULL TRAINING]===="
+  ./scripts/run-exp-simple.sh \
+  -c experiment_configs/simple_thesis.jsonnet \
+  -s model-output/ \
+  --num-epochs 2 \
+  --batch-size 4 \
+  --train-path thesis_data/preprocessed/data-train.p \
+  --dev-path thesis_data/preprocessed/data-val.p \
+  --num-train-instances 60 \
+  --cuda-device -1
+else
+
+  echo "Using demo samples for training.. ====[NOT THE REAL FULL DATA]===="
+  ./scripts/run-exp-simple.sh \
+  -c experiment_configs/simple_thesis.jsonnet \
+  -s model-output/ \
+  --num-epochs 2 \
+  --batch-size 4 \
+  --train-path thesis_data/preprocessed_demo/data-train.p \
+  --dev-path data/preprocessed_demo/data-val.p \
+  --num-train-instances 60 \
+  --cuda-device -1
+fi
+
+echo "DONE. Training finished.."
