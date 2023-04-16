778faee93bb7afd9874e2db33353d01ec3da702f
Olli Jokinen <olli.jokinen@snowfox.ai>
2023-04-16 12:17:48 +0200

    [THESIS-2] read me additions and changed script arguments

--- 3-create-training-files-thesis.sh
+++ 3-create-training-files-thesis.sh
@@ -11,7 +11,7 @@ if [[ $* == *--install_packages* ]]
 then
  echo "re building specter packages..."
  set -x
- python setup.py install > /dev/null
+ python setup.py install > /dev/null #not outputting the install lines
 else
   echo "not rebuilding specter packages"
 fi
@@ -22,22 +22,22 @@ set +x
 
 if [[ ! $* == *--demo* ]]
 then
-
   echo "Using full data for training..."
   python specter/data_utils/create_training_files.py \
     --data-dir thesis_data/training \
     --metadata thesis_data/training/metadata.json \
     --outdir thesis_data/preprocessed/
+    --bert_vocab thesis_data/finnish_bert_cased/vocab.txt
   echo "Done. See results is thesis_data/preprocessed/"
 
 else
 
-  echo "Using demo samples for training..."
+  echo "Using 100 DEMO samples for training..."
   python specter/data_utils/create_training_files.py \
     --data-dir thesis_data/training_demo/ \
-    --metadata thesis_data/training_demo/metadata.json \
+    --metadata thesis_data/training_demo/metadata_full.json \
     --outdir thesis_data/preprocessed_demo \
-    -bert_vocab thesis_data/finnish_bert_cased/vocab.txt
+    --bert_vocab thesis_data/finnish_bert_cased/vocab.txt
   echo "Done. See results is thesis_data/preprocessed_demo/"
 fi
 
--- README_THESIS.md
+++ README_THESIS.md
@@ -1,7 +1,33 @@
-## Training scrips 
+# Training scripts and data 
 
 
 ### Create training files
+When creating training files you need to provide three files 
+-  metadata.json 
+- data.json
+- train.txt, val.txt and test.txt 
+
+If you're creating only samples use filtered citation data but full metadata.
+It's important that the cited articles are found from the metadata file. 
+
 ```bash
+Demo version using only samples.
 ./3-create-training-files-thesis.sh --demo --install_packages
+
+Full version install packages: 
+./3-create-training-files-thesis.sh --install_packages
+
+Full version 
+./3-create-training-files-thesis.sh
+```
+
+
+### Run training script 
+
+```bash
+Demo version
+./4-run-training-script-thesis.sh --demo
+
+Full version 
+./4-run-training-script-thesis.sh
 ```
